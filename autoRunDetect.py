fbe50c8287b3d13bfb262491078be93a06650bb0
momo-xii <momo_instagram@163.com>
2018-01-12 09:59:32 +0800

    Detect if running, and autorun

---
+++ autoRunDetect.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-  
+# 定时检测程序是否还在运行，若没有运行则启动程序
+import sys
+import time
+import psutil
+import subprocess
+
+exeName = 'CQP.exe'
+CMD = "D:\\测试机器人.lnk"
+sleepTime = 15
+
+def searchProgram(programName):
+    exe_psutil = []
+    for p in psutil.process_iter():
+        try:
+            if p.name() == programName:
+                exe_psutil.append(p)
+        except psutil.Error:
+            pass
+    return exe_psutil
+
+if __name__ == "__main__":
+    while True:
+        foundProgram = searchProgram(exeName)
+        if foundProgram == []:
+            print("启动程序：%s"%(CMD))
+            subprocess.Popen('start /B %s'%(CMD), shell=True)
+        else:
+            print("程序正在运行：")
+            for p in foundProgram:
+                print(p.exe())
+        time.sleep(sleepTime)
